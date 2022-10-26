04ac40eb1e0dba584a76fe4a94b3b47123eafab3
lucas.scampos4 <lucassantoslukas2@gmail.com>
2022-10-26 11:03:33 -0300

    aula 26/10/22

--- ListaDuplamenteLigada/src/MetodosLista.java
+++ ListaDuplamenteLigada/src/MetodosLista.java
@@ -29,12 +29,53 @@ public class MetodosLista {
     }
 
     public static  Object[] pesquisar(String nome){
+        Elemento e = inicio;
+        String nomePesq;
+        while (e != null){
+            nomePesq = (String) e.getObjeto();
+
+            if (nome.equalsIgnoreCase(nomePesq)) {
+                Object[] v = new Object[2];
+                v[0] = e;
+                v[1] = nomePesq;
+                return v;
+            }
+            e = e.getProx();
+        }
         return null;
     }
 
     public static  boolean remover(String nome){
         Object [] obj = pesquisar(nome);
+        if (obj != null) {
+            Elemento e = (Elemento) obj[0];
 
+            //Remover inicio
+            if (e == inicio){
+                inicio = e.getProx();
+                //apaga a conexão com o proximo:
+                e.setProx(null);
+                //apaga a conexão com o anterior:
+                inicio.setAnt(null);
+                //ou
+                //e.getProx().setAnt(null);
+
+                //Remover atual:
+            } else if (e == atual) {
+                atual = e.getAnt();
+                aux = atual;
+                atual.setProx(null);
+                e.setAnt(null);
+
+                //Remover qualquer
+            }else {
+                e.getAnt().setProx(e.getProx());
+                e.getProx().setAnt(e.getAnt());
+                e.setProx(null);
+                e.setAnt(null);
+            }
+            return true;
+        }
         return false;
     }
 
--- ListaDuplamenteLigada/src/UsaLista.java
+++ ListaDuplamenteLigada/src/UsaLista.java
@@ -1,4 +1,10 @@
 public class UsaLista {
+    /**
+     * Elebore uma classe Pessoa:
+     * @param args(nome, id, idade). Armazene-os
+     * na lista dubla. Adapte o remover, para remover por id
+     * escreva o método atualizar por id.
+     */
 
     public static void main(String[] args) {
         MetodosLista.incerir("Gersom");
@@ -7,7 +13,14 @@ public class UsaLista {
         MetodosLista.incerir("José");
         MetodosLista.incerir("Maria");
 
+        if (MetodosLista.remover("Maria")){
+            System.out.println("Removeu");
+        }else {
+            System.out.println("Não removeu");
+        }
         MetodosLista.exibir();
+
+
     }
 
 }
